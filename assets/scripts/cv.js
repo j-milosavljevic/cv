95484a2b2fff084ce89fd94a025cea242545e946
Jovana Milosavljevic <jovanamilosav30@gmail.com>
2023-06-10 11:15:35 +0200

    Add files via upload
    init commit

---
+++ assets/scripts/cv.js
@@ -0,0 +1,34 @@
+function downloadFullCV() {
+  window.scrollTo({ top: 0 });
+
+  html2canvas($(".container"), {
+    useCORS: true,
+    pagesplit: true,
+
+    onrendered: function (canvas) {
+      console.log(canvas)
+      var doc = new jsPDF("p", "mm");
+      var imgData = canvas.toDataURL();
+
+      var imgHeight = (document.getElementById("container").getBoundingClientRect().height *25.4) / 96; //px to mm
+      var pageCount = Math.ceil(imgHeight / 297);
+
+      doc.addPage("l", "mm", "a4");
+      doc.addImage(imgData, "PNG", 2, 0);
+
+      if (pageCount >= 0) {
+        var j = 1;
+        while (j != pageCount) {
+          doc.addPage("l", "mm", "a4");
+          doc.addImage(imgData, "PNG", 2, -(j * 297));
+          j++;
+        }
+      }
+
+      doc.deletePage(1);
+
+      doc.save("jovana_milosavljevic_cv.pdf");
+      window.open(imgData);
+    },
+  });
+}
